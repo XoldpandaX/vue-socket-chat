6bc3650021d9f171a4558f86530db4ae60efff16
XoldpandaX <rembo-87@yandex.ru>
2019-12-21 15:50:19 +0700

    add: add new rules to prevent bad order of vue-component values

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,9 @@
 module.exports = {
   root: true,
   env: {
+    browser: true,
     node: true,
+    es6: true
   },
   extends: [
     'plugin:vue/essential',
@@ -10,6 +12,28 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'vue/order-in-components': ['error', {
+      order: [
+        'el',
+        'name',
+        'parent',
+        'functional',
+        ['delimiters', 'comments'],
+        ['components', 'directives', 'filters'],
+        'extends',
+        'mixins',
+        'inheritAttrs',
+        'model',
+        ['props', 'propsData'],
+        'LIFECYCLE_HOOKS',
+        'data',
+        'watch',
+        'computed',
+        'methods',
+        ['template', 'render'],
+        'renderError'
+      ]
+    }]
   },
   parserOptions: {
     parser: 'babel-eslint',

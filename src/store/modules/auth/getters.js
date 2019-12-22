e11c598b7e4e33fad1f526d61f5cdb5a4ebbc558
XoldpandaX <rembo-87@yandex.ru>
2019-12-23 00:16:47 +0700

    remove: delete excess module

--- src/store/modules/auth/getters.js
+++
@@ -1,4 +0,0 @@
-export default {
-  userName: ({ userName }) => userName,
-  userAvatar: ({ userAvatar }) => userAvatar,
-};
--- src/store/modules/auth/mutations.js
+++
@@ -1,8 +0,0 @@
-import * as mutationTypes from './mutation-types';
-
-export default {
-  [mutationTypes.SET_USER](state, { userName, userAvatar }) {
-    state.userName = userName;
-    state.userAvatar = userAvatar;
-  },
-};

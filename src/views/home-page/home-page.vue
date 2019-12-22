5aa7808182eb33cb12cf311f40ef3e0723bf0640
XoldpandaX <rembo-87@yandex.ru>
2019-12-22 18:29:20 +0700

    change: split home-page component into 3 diff parts && gather it in index.js

---
+++ src/views/home-page/home-page.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="home-page">
+    <h1>
+      Please enter you name<br/>
+      and choose avatar to enter chat ...
+    </h1>
+    <pre-chat-container />
+  </div>
+</template>
+
+<script>
+import { PreChatContainer } from '@/containers/pre-chat';
+
+export default {
+  name: 'home-page',
+  components: {
+    PreChatContainer,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+  @import 'home-page';
+</style>

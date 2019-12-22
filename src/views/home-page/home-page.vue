ed297b2a4dfa2c2274d4ab76b1729091b6cd99de
XoldpandaX <rembo-87@yandex.ru>
2019-12-22 19:57:34 +0700

    fix: small fix

--- src/views/home-page/home-page.vue
+++ src/views/home-page/home-page.vue
@@ -1,9 +1,6 @@
 <template>
   <div class="home-page">
-    <h1>
-      Please enter you name<br/>
-      and choose avatar to enter chat ...
-    </h1>
+    <h1>Please enter you name<br/>and choose avatar to enter chat ...</h1>
     <enter-form-container />
   </div>
 </template>

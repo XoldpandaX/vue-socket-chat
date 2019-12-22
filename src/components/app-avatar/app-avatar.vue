49098b074bf78016f641a0c65605696fb03baebc
XoldpandaX <rembo-87@yandex.ru>
2019-12-22 18:27:38 +0700

    add: add wrapper under default vuenes avatar component

---
+++ src/components/app-avatar/app-avatar.vue
@@ -0,0 +1,38 @@
+<template>
+  <div
+    class="app-avatar"
+    @click="clickHandler"
+  >
+    <nes-avatar
+      alt="user avatar"
+      :large="true"
+      :rounded="isRounded"
+      :src="src"
+    />
+  </div>
+</template>
+
+<script>
+import VueTypes from 'vue-types';
+
+export default {
+  name: 'app-avatar',
+  props: {
+    id: VueTypes.string.isRequired,
+    src: VueTypes.string.isRequired,
+    hasAction: VueTypes.bool.def(false),
+    isRounded: VueTypes.bool.def(false),
+  },
+  methods: {
+    clickHandler() {
+      if (this.hasAction) {
+        this.$emit('click-avatar', this.id);
+      }
+    },
+  },
+};
+</script>
+
+<style scoped>
+
+</style>

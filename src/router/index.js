80caf96ea5b3c5af842001c02e0008c38383ba7c
XoldpandaX <rembo-87@yandex.ru>
2019-12-22 16:01:14 +0700

    change: rename components && change component for certain routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import Home from '../views/Home.vue';
+import About from '@/views/About.vue';
 
 Vue.use(VueRouter);
 
@@ -8,15 +8,15 @@ const routes = [
   {
     path: '/',
     name: 'home',
-    component: Home,
+    component: About,
   },
   {
-    path: '/about',
-    name: 'about',
+    path: '/chat',
+    name: 'chat',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    component: () => import(/* webpackChunkName: "about" */ '../views/Chat.vue'),
   },
 ];
 

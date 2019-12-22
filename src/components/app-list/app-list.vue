04d9b8b4c00fac11f83bd2c18e209d4b0a620c7f
XoldpandaX <rembo-87@yandex.ru>
2019-12-22 18:28:09 +0700

    add: add universal list component

---
+++ src/components/app-list/app-list.vue
@@ -0,0 +1,23 @@
+<template>
+  <ul>
+    <li
+      v-for="listItem in listItems"
+      :key="listItem.id"
+      :class="itemsClasses"
+    >
+      <slot :item="listItem" />
+    </li>
+  </ul>
+</template>
+
+<script>
+import VueTypes from 'vue-types';
+
+export default {
+  name: 'app-list',
+  props: {
+    listItems: VueTypes.array.isRequired,
+    itemsClasses: VueTypes.array.def([]),
+  },
+};
+</script>

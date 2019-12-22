121e7ba96a17cf2ef27a85362d9b31dd4a342f69
XoldpandaX <rembo-87@yandex.ru>
2019-12-22 16:00:43 +0700

    add: install vuenes plugin into app

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 import Vue from 'vue';
 import router from './router';
 import createStore from '@/store';
-import installVueNativeWebsocket from '@/plugins/vue-native-websocket';
 import * as mutationTypes from '@/store/modules/chat-socket/mutation-types';
 import { SOCKET_ENDPOINT } from './constants';
+
+// plugins
+import installVueNativeWebsocket from '@/plugins/vue-native-websocket';
+import installVueNesCss from '@/plugins/vue-nes-css';
 
 import App from './App.vue';
 
@@ -14,6 +17,7 @@ installVueNativeWebsocket({
   endpoint: SOCKET_ENDPOINT,
   moduleName: 'chat-socket',
 });
+installVueNesCss();
 
 Vue.config.productionTip = false;
 new Vue({

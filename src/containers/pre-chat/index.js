9c04693308d5cd68be5f582f5b55de38c8d1c33a
XoldpandaX <rembo-87@yandex.ru>
2019-12-22 18:26:51 +0700

    add: add container folder with pre-chat container

---
+++ src/containers/pre-chat/index.js
@@ -0,0 +1,1 @@
+export { default as PreChatContainer } from './pre-chat-container.vue';
---
+++ src/containers/pre-chat/pre-chat-container.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="pre-chat-container">
+    <enter-form @form-submitted="c" />
+    <avatar-chooser @avatar-chosen="r" />
+  </div>
+</template>
+
+<script>
+import { AvatarChooser } from '@/components/avatar-chooser';
+import { EnterForm } from '@/components/enter-form';
+
+export default {
+  name: 'pre-chat-container',
+  components: {
+    AvatarChooser,
+    EnterForm,
+  },
+  methods: {
+    c(userName) {
+      console.info(userName);
+    },
+    r(avatarId) {
+      console.info(avatarId);
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+  @import 'pre-chat-container.scss';
+</style>

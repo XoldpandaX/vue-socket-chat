cf2f2ba77365625686bd3b261ea8e293f1c9c60c
XoldpandaX <rembo-87@yandex.ru>
2019-12-22 19:51:16 +0700

    add: add new constant with avatar images

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,13 @@
+import image1 from '@/assets/images/avatar1.jpg';
+import image2 from '@/assets/images/avatar2.png';
+import image3 from '@/assets/images/avatar3.jpg';
+import image4 from '@/assets/images/avatar4.jpg';
+
+export const CHAT_AVATARS = {
+  image1,
+  image2,
+  image3,
+  image4,
+};
+
 export const SOCKET_ENDPOINT = 'ws://pm.tada.team/ws';

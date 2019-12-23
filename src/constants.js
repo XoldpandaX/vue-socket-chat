106a15be3e0e024d867deec13abf831817133c57
XoldpandaX <rembo-87@yandex.ru>
2019-12-23 10:00:04 +0700

    add: add new avatar to chat avatars constant

--- src/constants.js
+++ src/constants.js
@@ -2,12 +2,14 @@ import image1 from '@/assets/images/avatar1.jpg';
 import image2 from '@/assets/images/avatar2.png';
 import image3 from '@/assets/images/avatar3.jpg';
 import image4 from '@/assets/images/avatar4.jpg';
+import image5 from '@/assets/images/avatar5.jpeg';
 
 export const CHAT_AVATARS = {
   image1,
   image2,
   image3,
   image4,
+  image5,
 };
 
 export const CHAT_MESSAGE_TYPES = {

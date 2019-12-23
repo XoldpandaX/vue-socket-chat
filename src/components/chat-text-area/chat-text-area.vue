227ebba5bf73b5b1ac8c91245ebb012479d7e240
XoldpandaX <rembo-87@yandex.ru>
2019-12-23 09:59:10 +0700

    add: add neew logic to sens msg on enter and linebreak on ctrl+enter && small additions

--- src/components/chat-text-area/chat-text-area.vue
+++ src/components/chat-text-area/chat-text-area.vue
@@ -1,16 +1,19 @@
 <template>
   <div class="chat-text-area">
-    <nes-input-text-area
-      id="message"
-      :init-value="textAreaValue"
-      :cols="10"
-      :rows="10"
-      @input="setTextAreaValue"
-    />
+    <div class="field">
+      <textarea
+        class="nes-textarea"
+        ref="chatTextarea"
+        autocomplete="off"
+        v-model="inputText"
+        @input="$emit('input', inputText)"
+        @keypress="handleTextAreaBehavior"
+      />
+    </div>
     <div class="chat-text-area-send-btn">
       <nes-button
         :success="true"
-        @click="$emit('submit-btn-click', textAreaValue)"
+        @click="sendMessage"
       >
         send
       </nes-button>
@@ -19,18 +22,43 @@
 </template>
 
 <script>
-// import VueTypes from 'vue-types';
+import VueTypes from 'vue-types';
 
 export default {
   name: 'chat-text-area',
+  props: {
+    initValue: VueTypes.string.isRequired,
+  },
   data() {
     return {
-      textAreaValue: '',
+      inputText: this.initValue,
     };
   },
   methods: {
-    setTextAreaValue(text) {
-      this.textAreaValue = text;
+    sendMessage() {
+      this.$emit('submit-btn-click', this.inputText);
+      this.inputText = '';
+    },
+    addLineBreakToInputText() {
+      const strArr = this.inputText.split('');
+      strArr.push('\n');
+      this.inputText = strArr.join('');
+    },
+    handleTextAreaBehavior(e) {
+      // scroll to bottom on new line
+      this.$refs.chatTextarea.scrollTop = this.$refs.chatTextarea.scrollHeight;
+
+      const actionByKeyCode = {
+        13: () => {
+          e.preventDefault();
+          this.sendMessage();
+        },
+        10: () => {
+          this.addLineBreakToInputText();
+        },
+      };
+
+      return actionByKeyCode[e.keyCode] ? actionByKeyCode[e.keyCode]() : null;
     },
   },
 };
